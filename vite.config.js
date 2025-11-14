758ed73db941d3486e3cb1b64ed57e6b5471ff3e
Elizomar <elizomar.kc@gmail.com>
2025-11-14 10:51:08 -0400

    Ajuste de corns

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+const repositoryName = process.env.GITHUB_REPOSITORY?.split('/')[1]
+const basePath =
+  process.env.GITHUB_ACTIONS && repositoryName ? `/${repositoryName}/` : '/'
+
 // https://vite.dev/config/
 export default defineConfig({
+  base: basePath,
   plugins: [react()],
 })
